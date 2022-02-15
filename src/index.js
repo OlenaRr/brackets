1e09992eb5234ba08e921b5d9657ae6061aa84c9
OlenaRr <olenkar093@gmail.com>
2022-02-15 22:24:55 +0200

    feat: added solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let pair = {};
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    pair[bracketsConfig[i][0]] = bracketsConfig[i][1];
+  }
+  console.log(pair)
+  let check = [];
+  for (let i = 0; i < str.length; i++) {
+      let current = str[i];
+      console.log(current, check)
+      if (pair[current] !== undefined) {
+        if (current !== pair[current] || check[check.length - 1] !== current) {
+          check.push(current);
+        } else {
+          check.pop();
+        }
+      } else {
+        if (Object.values(pair).indexOf(current) !== -1) {
+          console.log(check)
+          console.log(Object.values(pair).indexOf(current), Object.keys(pair)[Object.values(pair).indexOf(current)], pair[Object.keys(pair)[Object.values(pair).indexOf(current)]])
+          if (check[check.length - 1] === Object.keys(pair)[Object.values(pair).indexOf(current)]) {
+            check.pop()
+          } else {return false}
+        } else {return false}
+      }
+  }
+  return check.length === 0
 }
+
+
